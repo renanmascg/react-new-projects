76f503a00bd4130ea30101c3d6100ca86855e534
renanmascg <renanmascg@gmail.com>
2022-03-29 12:28:23 -0300

    Adiciona o auto refresh da aplicação quando rodar ela via webpack

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   mode: "development",
@@ -10,6 +11,12 @@ module.exports = {
   resolve: {
     extensions: [ '.js', '.jsx' ] // naturalmente só entende js, falamos para entender, também .jsx
   },
+  devServer: {
+    static: path.resolve(__dirname, 'public')
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, "public", "index.html")
+  })],
   module: { // instruções de como organizar os arquivos
     rules: [
       {
