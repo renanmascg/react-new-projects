23e86c1c09ec39aeb3a15e78ab7db06211551fed
renanmascg <renanmascg@gmail.com>
2022-03-29 11:55:27 -0300

    Initial Commit

---
+++ 01-github-explorer/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path")
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.jsx'), // arquivo de entrada
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [ '.js', '.jsx' ] // naturalmente só entende js, falamos para entender, também .jsx
+  },
+  module: { // instruções de como organizar os arquivos
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ],
+  }
+}
