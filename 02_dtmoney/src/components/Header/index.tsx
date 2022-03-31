2c596461e940d293c37c29f26cd04ae0be02078d
renanmascg <renanmascg@gmail.com>
2022-03-31 10:40:34 -0300

    Termina a estilização do modal dentro da aplicação.

--- 02_dtmoney/src/components/Header/index.tsx
+++ 02_dtmoney/src/components/Header/index.tsx
@@ -1,14 +1,21 @@
+import { useState } from 'react'
+import Modal from 'react-modal';
 import LogoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({onOpenNewTransactionModal}: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={LogoImg} alt="dt money" />
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransactionModal}>
           Nova Transação
         </button>
+
       </Content>
     </Container>
   )
