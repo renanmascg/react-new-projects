b8d4a0395d4461025f988774f7e82963f7ba11f7
renanmascg <renanmascg@gmail.com>
2022-03-31 14:00:06 -0300

    Adiciona properties nos styled components e termina a parte do formulário dentro dele.

--- 02_dtmoney/src/components/NewTransactionModal/index.tsx
+++ 02_dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,13 +1,18 @@
+import { useState } from 'react';
 import Modal from 'react-modal';
-import closeImg from '../../assets/close.svg'
-import { Container } from './styles';
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, RadioBox, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
 }
 
-export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionModalProps) {  
+export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit');
+
   return (
       <Modal 
           isOpen={isOpen} 
@@ -31,6 +36,30 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
           <input type="number" 
             placeholder='Valor'
           />
+
+          <TransactionTypeContainer>
+            
+            <RadioBox
+              type='button'
+              onClick={() => setType('deposit')}
+              isActive={type === 'deposit'}
+              activeColor="green"
+            >
+              <img src={incomeImg} alt="Entrada" />
+              <span>Entrada</span>
+            </RadioBox>
+            
+            <RadioBox
+              type='button'
+              onClick={() => setType('withdraw')}
+              isActive={type === 'withdraw'}
+              activeColor="red"
+            >
+              <img src={outcomeImg} alt="Saída" />
+              <span>Saída</span>
+            </RadioBox>
+
+          </TransactionTypeContainer>
 
           <input type="text" placeholder='Categoria'/>
           <button type="submit">
