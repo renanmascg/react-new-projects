373216fd87d680bd4effb340b07891eb71b5cba9
renanmascg <renanmascg@gmail.com>
2022-04-04 15:15:19 -0300

    Externaliza a geração do useContext para o nosso hook criado.

--- 02_dtmoney/src/components/NewTransactionModal/index.tsx
+++ 02_dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,13 +1,12 @@
 import Modal from 'react-modal';
-import  { FormEvent, useState, useContext } from 'react';
+import  { FormEvent, useState } from 'react';
 
-import { api } from '../../services/api';
-import { TransactionsContext } from '../../TransactionsContext';
 import { Container, RadioBox, TransactionTypeContainer } from './styles';
 
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
+import { useTransactions } from '../../hooks/useTransactions';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -15,7 +14,7 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionModalProps) {
-  const { createTransaction } = useContext(TransactionsContext);
+  const { createTransaction } = useTransactions();
 
   const [type, setType] = useState('deposit');
   const [title, setTitle] = useState('')
