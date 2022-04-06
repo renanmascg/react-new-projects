6915b24261e64190823fec0dc29af9a985b9af73
renanmascg <renanmascg@gmail.com>
2022-04-06 16:52:04 -0300

    Adiciona o botão de subscribe dentro da aplicação.

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import styles from './home.module.scss';
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
             Get Access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
         
         <img src="/images/avatar.svg" alt="Girl Coding" />
