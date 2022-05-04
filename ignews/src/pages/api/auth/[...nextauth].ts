563fa17e21dbef06d8e7e89fe515ffdabe5502c4
renanmascg <renanmascg@gmail.com>
2022-05-04 15:53:01 -0300

    Adiciona o signin do usuário, salvando as informações dele no banco de dados caso o usuário não existir

--- ignews/src/pages/api/auth/[...nextauth].ts
+++ ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+import { query } from "faunadb"
+import { fauna } from "../../../services/fauna";
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -14,4 +17,41 @@ export default NextAuth({
       }
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index("user_by_email"),
+                  query.Casefold(email)
+                )
+              )
+            ),
+            query.Create(
+              query.Collection("users"),
+              {
+                data: { email }
+              }
+            ),
+            query.Get( 
+              query.Match(
+                query.Index("user_by_email"),
+                query.Casefold(email)
+              )
+            )
+          )
+        )
+
+        return true
+      } catch (err) {
+        console.log(err)
+        return false;
+      }
+    },
+  }
 })
