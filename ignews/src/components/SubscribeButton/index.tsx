4930d15348c73fcb096c175d8713a0b417d9e644
renanmascg <renanmascg@gmail.com>
2022-05-05 10:44:23 -0300

    Configura o frontend para redirecionar para a parte de pagamentos da aplicação

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJS } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,9 +8,37 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+
+  const {data: session} = useSession();
+
+  async function handleSubscribe() {
+    // validar se o usuário estiver logado, se não estiver, redireciona ele para um signin
+    if(!session) {
+      signIn();
+      return;
+    }
+
+    // criação da checkout session
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJS();
+
+      await stripe.redirectToCheckout({
+        sessionId
+      });
+    } catch(err) { 
+      alert(err.message);
+    }
+
+  }
+
   return (
     <button type='button'
     className={styles.subscribeButton}
+    onClick={handleSubscribe}
     >
       Subscribe now
     </button>
