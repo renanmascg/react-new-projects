5f0b1f6f94fee3e727981eca4442fcbc2435202d
renanmascg <renanmascg@gmail.com>
2022-05-11 15:25:19 -0300

    Termina o layout da aplicação e suas páginas, sem conexaõ com APIs e etc

--- 04_dashgo/src/pages/dashboard.tsx
+++ 04_dashgo/src/pages/dashboard.tsx
@@ -86,6 +86,7 @@ export default function Dashboard() {
             borderRadius={8}
           >
             <Text fontSize="lg" mb="4">  Taxa de Abertura</Text>
+            <Chart options={options} series={series} type="area" height={160} />
           </Box>
 
         </SimpleGrid>
---
+++ 04_dashgo/src/pages/users/index.tsx
@@ -0,0 +1,70 @@
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+      <Flex width="100%" my="5" maxWidth={1400} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex='1' borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Button
+              as="a"
+              size="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar Novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink"></Checkbox>
+                </Th>
+                <Th>Usuários </Th>
+                <Th>Data de Cadastro</Th>
+                <Th width="8"></Th>
+              </Tr>
+            </Thead>
+
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink"></Checkbox>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Renan Mascarenhas</Text>
+                    <Text fontWeight="normal" fontSize="sm" color="gray.300">renanmascg@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td> 04 de Abril de 2021</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine}  fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+          
+          <Pagination />
+        </Box>
+      </Flex>
+    </Box >
+  );
+}
